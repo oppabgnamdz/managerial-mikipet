c474d2162d26e3119795dcb63c3ff6afd5ae9a6a
NGUYENHOANGNAM <namnn@vn-sis.comnhnobnd@gmail.com>
2021-07-15 23:18:07 +0700

    design main 1

---
+++ src/component/Header/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { TiGroup } from 'react-icons/ti';
+import { IoDocuments } from 'react-icons/io5';
+import { FaUserTimes } from 'react-icons/fa';
+import { GoReport } from 'react-icons/go';
+import { IoLogOut } from 'react-icons/io5';
+import './style.scss';
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header__statistic">
+        <h1>Thống Kê</h1>
+        <a href="#">
+          <IoLogOut className="icon-log-out" />
+        </a>
+      </div>
+      <div className="header__route">
+        <a href="#" className="header__route-flex">
+          <TiGroup className="header__route-flex-icon users" />
+          <div className="header__route-flex-text">
+            <span lassName="header__route-flex-text-title">
+              Số lượng người dùng
+            </span>
+            <span className="header__route-flex-text-number">99</span>
+          </div>
+        </a>
+        <a href="#" className="header__route-flex">
+          <IoDocuments className="header__route-flex-icon posts" />
+          <div className="header__route-flex-text">
+            <span lassName="header__route-flex-text-title">
+              Số lượng bài viết
+            </span>
+            <span className="header__route-flex-text-number">250</span>
+          </div>
+        </a>
+        <a href="#" className="header__route-flex">
+          <FaUserTimes className="header__route-flex-icon user-report" />
+          <div className="header__route-flex-text">
+            <span lassName="header__route-flex-text-title">
+              Người dùng báo cáo
+            </span>
+            <span className="header__route-flex-text-number">06</span>
+          </div>
+        </a>
+        <a href="#" className="header__route-flex">
+          <GoReport className="header__route-flex-icon post-report" />
+          <div className="header__route-flex-text">
+            <span className="header__route-flex-text-title">
+              Bài viết báo cáo
+            </span>
+            <span className="header__route-flex-text-number">03</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ src/component/Navigation/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from '../../asset/logo.png';
+import './style.scss';
+import { IoHome } from 'react-icons/io5';
+import { IoDocuments } from 'react-icons/io5';
+import { FaUserTimes } from 'react-icons/fa';
+import { GoReport } from 'react-icons/go';
+export default function index() {
+  return (
+    <div className="nav">
+      <div className="intro">
+        <img src={logo} alt="Logo" />
+        <span className="intro-name">Miki</span>
+        <span>Tận hưởng thế giới thú cưng của bạn</span>
+      </div>
+      <div className="navigate">
+        <div className="navigate-home">
+          <a href="#">
+            <IoHome />
+            <span className="navigate-home-span">Trang chủ</span>
+          </a>
+        </div>
+        <div className="navigate-home">
+          <a href="#">
+            <IoDocuments />
+            <span className="navigate-home-span">Số lượng bài viết</span>
+          </a>
+        </div>
+        <div className="navigate-home">
+          <a href="#">
+            <FaUserTimes />
+            <span className="navigate-home-span">Người dùng báo cáo</span>
+          </a>
+        </div>
+        <div className="navigate-home">
+          <a href="#">
+            <GoReport />
+            <span className="navigate-home-span">Bài viết báo cáo</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
