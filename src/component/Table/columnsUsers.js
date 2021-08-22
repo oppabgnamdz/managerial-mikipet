caa5debc4204ac0c18f4274c95e23f265ad99c69
NGUYENHOANGNAM <namnn@vn-sis.comnhnobnd@gmail.com>
2021-08-22 12:50:07 +0700

    update reported

--- src/component/Table/columnsUsers.js
+++ src/component/Table/columnsUsers.js
@@ -1,30 +1,34 @@
 export const COLUMNS = [
-  {
-    Header: 'Id',
-    accessor: '_id',
-  },
-  {
-    Header: 'Account',
-    accessor: 'account',
-  },
-  {
-    Header: 'Create at',
-    accessor: 'created_date',
-  },
-  {
-    Header: 'Email',
-    accessor: 'email',
-  },
-  {
-    Header: 'Birth day',
-    accessor: 'birthDay',
-  },
-  {
-    Header: 'Display Name',
-    accessor: 'displayName',
-  },
-  {
-    Header: 'Active',
-    accessor: (d) => (d.active ? 'active' : 'locked'),
-  },
+	{
+		Header: 'Id',
+		accessor: '_id',
+	},
+	{
+		Header: 'Account',
+		accessor: 'account',
+	},
+	{
+		Header: 'Create at',
+		accessor: 'created_date',
+	},
+	{
+		Header: 'Email',
+		accessor: 'email',
+	},
+	{
+		Header: 'Reported',
+		accessor: (d) => (d.reported ? d.reported.length : 'undefined'),
+	},
+	{
+		Header: 'Birth day',
+		accessor: 'birthDay',
+	},
+	{
+		Header: 'Display Name',
+		accessor: 'displayName',
+	},
+	{
+		Header: 'Active',
+		accessor: (d) => (d.active ? 'active' : 'locked'),
+	},
 ];
